3150d89750161e61ef1da7db884d19ca45e4913e
isubasinghe <isubasinghe@student.unimelb.edu.au>
2021-12-20 11:24:36 +1100

    added GOPATH and go directories

--- scripts/install_node.sh
+++ scripts/install_node.sh
@@ -7,3 +7,19 @@ wget https://go.dev/dl/go1.17.5.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.17.5.linux-amd64.tar.gz
 
 echo "export PATH=\"$PATH:/usr/local/go/bin\"" >> ~/.bashrc
+echo "export GOPATH=/home/$USER/go" >> ~/.bashrc
+
+HOSTS=`cat <<EOF
+127.0.0.1 dex
+127.0.0.1 minio
+127.0.0.1 postgres
+127.0.0.1 mysql
+EOF
+`
+
+sudo -- sh -c "echo \"$HOSTS\" >> /etc/hosts" 
+
+mkdir /home/$USER/go
+mkdir /home/$USER/go/src
+mkdir /home/$USER/go/src/github.com
+mkdir /home/$USER/go/src/github.com/argoproj
